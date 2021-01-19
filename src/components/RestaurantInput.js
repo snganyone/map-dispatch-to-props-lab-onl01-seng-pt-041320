03dc94c936fa1a1740c69388af4989bbd846993f
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-01-18 21:22:06 -0800

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.dispatch({type: 'ADD_RESTAURANT', restaurant: this.state})
   }
 
   render() {
@@ -51,4 +51,4 @@ export class RestaurantInput extends Component {
 
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect()(RestaurantInput);
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -5,7 +5,7 @@ class Restaurants extends Component {
 
   render() {
 
-    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
+    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name} Location: {restaurant.location}</li>);
 
     return (
       <div>
